aef5028df4d65779e9022ce1270f0b0e7cdb3b30
saurav00679 <sauravsharma.00679@gmail.com>
2023-09-15 14:31:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res)=>{
+    try{
+        const result = await axios.get('https://dog.ceo/api/breeds/image/random');
+        res.render('index.ejs', { img: result.data.message });
+    } catch(error){
+        console.log(error.response.data);
+        res.status(500);
+    }
+
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started at port no. ${port}`);
+})
